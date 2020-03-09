91486276a14735cfaa9b58041bc29e42bffeec8f
Thomas Hess <thomas.hess@udo.edu>
2020-03-09 15:52:18 +0100

    Implemented parsing of the optional activity and bitmap mdstat lines.

--- ksysguard_mdraid_monitor/model.py
+++ ksysguard_mdraid_monitor/model.py
@@ -51,48 +51,73 @@ class RaidDeviceInfo:
         self.block_count, self.superblock_format, self.chunk_size, self.expected_device_count, \
             self.current_device_count = self._parse_block_count_line(line_2)
 
-        print(line_1)
-        print(line_2)
-        print(line_3)
-        print(line_4)
+        # Set defaults for optional parts
+        self.current_activity = "idle"
+        self.progress_percent = 0.0
+        self.currently_processed_block = 0
+        self.activity_eta_minutes = 0.0
+        self.speed_kbytes_per_sec = 0
+
+        self.has_bitmap = False
+        self.bitmap_used_pages = 0
+        self.bitmap_total_pages = 0
+        self.bitmap_used_size_kb = 0
+        self.bitmap_chunk_size_kb = 0
 
         if line_3:
             if line_3.startswith("bitmap"):
-                self._parse_bitmap_line(line_3)
+                # Device has a bitmap and is currently idle
+                self.has_bitmap = True
+                self.bitmap_used_pages, self.bitmap_total_pages, self.bitmap_used_size_kb, self.bitmap_chunk_size_kb = \
+                    self._parse_bitmap_line(line_3)
             else:
-                # Recovery, resync or check in progress
-                self._parse_activity_line(line_3)
+                # Device has no bitmap and a recovery, resync or check in progress
+                self.current_activity, self.progress_percent, self.currently_processed_block, \
+                    self.activity_eta_minutes, self.speed_kbytes_per_sec = self._parse_activity_line(line_3)
         
         if line_4:
-            # Recovery, resync or check in progress, and the device has a bitmap
-            self._parse_bitmap_line(line_4)
+            # Device has a bitmap and a recovery, resync or check in progress
+            self.has_bitmap = True
+            self.bitmap_used_pages, self.bitmap_total_pages, self.bitmap_used_size_kb, self.bitmap_chunk_size_kb = \
+                self._parse_bitmap_line(line_4)
     
     @staticmethod
-    def _parse_header_line(line_1: str):
-        line_1_result = header_parser.match(line_1)
-        md_device = line_1_result.group("md_device")
-        is_active = line_1_result.group("is_active") == "active"
-        raid_level = line_1_result.group("level")
-        component_devices = line_1_result.group("components").strip().split(" ")
+    def _parse_header_line(header_line: str):
+        header_result = header_parser.match(header_line)
+        md_device = header_result.group("md_device")
+        is_active = header_result.group("is_active") == "active"
+        raid_level = header_result.group("level")
+        component_devices = header_result.group("components").strip().split(" ")
         return md_device, is_active, raid_level, component_devices
     
     @staticmethod
-    def _parse_block_count_line(line_2: str):
-        line_2_result = block_parser.match(line_2)
-        block_count = int(line_2_result.group("block_count"))
-        superblock_format = line_2_result.group("superblock_format")
-        chunk_size = line_2_result.group("chunk_size")
-        expected_device_count = int(line_2_result.group("expected_device_count"))
-        current_device_count = int(line_2_result.group("current_device_count"))
+    def _parse_block_count_line(block_count_line: str):
+        block_count_result = block_parser.match(block_count_line)
+        block_count = int(block_count_result.group("block_count"))
+        superblock_format = block_count_result.group("superblock_format")
+        chunk_size = block_count_result.group("chunk_size")
+        expected_device_count = int(block_count_result.group("expected_device_count"))
+        current_device_count = int(block_count_result.group("current_device_count"))
         return block_count, superblock_format, chunk_size, expected_device_count, current_device_count
 
     @staticmethod
-    def _parse_activity_line(line_3: str):
-        pass
+    def _parse_activity_line(activity_line: str):
+        activity_result = activity_parser.match(activity_line)
+        current_activity = activity_result.group("activity_mode")
+        progress_percent = float(activity_result.group("progress"))
+        currently_processed_block = int(activity_result.group("current_block"))
+        activity_eta_minutes = float(activity_result.group("eta_min"))
+        speed_kbytes_per_sec = int(activity_result.group("speed"))
+        return current_activity, progress_percent, currently_processed_block, activity_eta_minutes, speed_kbytes_per_sec
 
     @staticmethod
-    def _parse_bitmap_line(line_3: str):
-        pass
+    def _parse_bitmap_line(bitmap_line: str):
+        bitmap_result = bitmap_parser.match(bitmap_line)
+        bitmap_used_pages = int(bitmap_result.group("used_pages_count"))
+        bitmap_total_pages = int(bitmap_result.group("total_pages_count"))
+        bitmap_used_size_kb = int(bitmap_result.group("size_used_kb"))
+        bitmap_chunk_size_kb = int(bitmap_result.group("bitmap_chunk_size_kb"))
+        return bitmap_used_pages, bitmap_total_pages, bitmap_used_size_kb, bitmap_chunk_size_kb
 
     @property
     def component_count(self) -> int:
@@ -154,3 +179,7 @@ class RaidStatus:
     @property
     def total_component_count(self) -> int:
         return sum(device.component_count for device in self.device_info)
+
+    @property
+    def bitmap_device_count(self) -> int:
+        return sum(1 for device in self.device_info if device.has_bitmap)
